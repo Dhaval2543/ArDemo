5da363ee6bf999bddcc4c11ea7a56db5acc25924
Dhaval Vekariya <dhaval.vekariya@softices.com>
2022-10-08 10:21:02 +0530

    new logo and splash screen changes

--- src/screen/ItemDetail.js
+++ src/screen/ItemDetail.js
@@ -360,7 +360,7 @@ export function ItemDetail({navigation, route}) {
                   const modelUrl =
                     params.item == 1
                       ? 'https://github.com/Dhaval2543/ArDemo/blob/main/res/leather_couch.glb?raw=true'
-                      : 'https://github.com/Dhaval2543/ArDemo/blob/main/res/Arm_chair_Furniture.glb?raw=true';
+                      : 'https://github.com/Dhaval2543/ArDemo/blob/main/res/furniture_for_real-time_visualization_engine.glb?raw=true';
                   ArViewerModule.openViewer(modelUrl, 'AR Viewer');
                 }
               }}>
